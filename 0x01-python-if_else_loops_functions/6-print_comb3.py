f1f3eb14b89823702e3acaaed78495400c68fba5
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-10-03 04:07:32 +0000

    all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:01d}{:01d}".format(i, j))
+        else:
+            print("{:01d}{:01d}, ".format(i, j), end="")
