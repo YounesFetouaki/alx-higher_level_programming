523d67640967ea73765788a2333e39bc346e96b8
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-10-05 13:13:16 +0000

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-
-def calculate(a, operator, b):
-    if operator == '+':
-        return a + b
-    elif operator == '-':
-        return a - b
-    elif operator == '*':
-        return a * b
-    elif operator == '/':
-        return a / b
-    else:
-        return None
-
 if __name__ == "__main__":
     import sys
-
-    if len(sys.argv) != 4:
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
     a = int(sys.argv[1])
-    operator = sys.argv[2]
     b = int(sys.argv[3])
-
-    result = calculate(a, operator, b)
-
-    if result is None:
+    av = sys.argv[2]
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
+    if av not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
-    print("{} {} {} = {}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, av, b, operator[av](a, b)))
