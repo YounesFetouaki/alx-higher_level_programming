e4dbd5fdda7cd1ad85c2dc7c3b7e2c1151ccac72
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2024-04-09 21:06:10 +0000

    :)

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,33 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const [, , fileA, fileB, fileC] = process.argv;
-
-// Read content of fileA
-fs.readFile(fileA, 'utf8', (err, dataA) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    // Read content of fileB
-    fs.readFile(fileB, 'utf8', (err, dataB) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        // Concatenate contents of fileA and fileB
-        const concatenatedContent = `${dataA.trim()}\n${dataB.trim()}\n`;
-
-        // Write concatenated content to fileC
-        fs.writeFile(fileC, concatenatedContent, err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(`${fileA} and ${fileB} have been concatenated into ${fileC}`);
-        });
-    });
-});
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], file1 + file2);
