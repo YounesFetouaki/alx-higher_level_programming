c8b6542eb27198abd7c3c1d4e8ae313dd9a426db
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2024-04-09 21:03:14 +0000

    :)

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+const { list } = require('./100-data');
 
-const list = require('./100-data').list;
+const mappedList = list.map((value, index) => value * index);
+
 console.log(list);
-console.log(list.map((x, index) => x * index));
+console.log(mappedList);
