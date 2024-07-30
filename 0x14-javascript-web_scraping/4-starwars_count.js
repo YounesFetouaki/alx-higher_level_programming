cf9fc6276da54740d19fbf44e0ed214d756df692
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2024-07-30 23:55:36 +0000

    0x14

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,28 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-// Get the API URL from the command line arguments
-const apiUrl = process.argv[2];
-
-// Define the character ID for Wedge Antilles
-const characterId = 18;
-
-// Make a GET request to the Star Wars API
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    // Print the error if one occurred
-    console.error(error);
-  } else {
-    // Parse the response body (string) into a JavaScript object
-    const data = JSON.parse(body);
-
-    // Filter the movies to include only those where Wedge Antilles is present
-    const moviesWithWedge = data.results.filter((movie) =>
-      movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-
-    // Print the number of movies where Wedge Antilles is present
-    console.log(moviesWithWedge.length);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
-
